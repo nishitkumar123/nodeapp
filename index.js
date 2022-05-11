94d4ef4e40081d56895129d51e7b1b0b51083687
nishitkumar123 <68488770+nishitkumar123@users.noreply.github.com>
2022-05-11 13:00:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const app = require('express')();
+const winston = require('winston');
+
+// Imports the Google Cloud client library for Winston
+const {LoggingWinston} = require('@google-cloud/logging-winston');
+
+// Creates a client
+const loggingWinston = new LoggingWinston({
+  projectId: 'la-gcp-test',
+  keyFilename: 'projects/la-gcp-test/log-test-bucket/winston_log.json',
+});
+
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+      new winston.transports.Console(),
+      // Add Stackdriver Logging
+      loggingWinston,
+    ],
+  });
+  
+  // Writes some log entries
 
 app.get('/', (req, res ) => 
-    res.json({ message: 'Docker is very easy 🐳' }) 
+    res.json({ message: 'Docker is easy 🐳' }) 
+    
 );
-
+logger.info('shields at 99%');
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
+
