22d0636907b7cb525b1895bcdfe2a4673be6776e
nishitkumar123 <68488770+nishitkumar123@users.noreply.github.com>
2022-05-11 16:14:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const app = require('express')();
-// Imports the Google Cloud client library for Winston
-/*const {LoggingWinston} = require('@google-cloud/logging-winston');
 
-// Creates a client
-const loggingWinston = new LoggingWinston({
-  projectId: 'la-gcp-test',
-  keyFilename: 'projects/la-gcp-test/log-test-bucket/winston_log.json',
-});
+// Imports the Google Cloud client library
+const {Logging} = require('@google-cloud/logging');
 
-const logger = winston.createLogger({
-    level: 'info',
-    transports: [
-      new winston.transports.Console(),
-      // Add Stackdriver Logging
-      loggingWinston,
-    ],
-  });
-  
-  // Writes some log entries */
+async function quickstart(
+  projectId = 'la-gcp-test', // Your Google Cloud Platform project ID
+  logName = 'nodejslog' // The name of the log to write to
+) {
+  // Creates a client
+  const logging = new Logging({projectId});
+
+  // Selects the log to write to
+  const log = logging.log(logName);
+
+  // The data to write to the log
+  const text = 'Hello, world!';
+
+  // The metadata associated with the entry
+  const metadata = {
+    resource: {type: 'global'},
+    // See: https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
+    severity: 'INFO',
+  };
+
+  // Prepares a log entry
+  const entry = log.entry(metadata, text);
+
+  async function writeLog() {
+    // Writes the log entry
+    await log.write(entry);
+    console.log(`Logged: ${text}`);
+  }
+  writeLog();
+}
+
 
 app.get('/', (req, res ) => 
     res.json({ message: 'Docker is easy 🐳' }) 
-    
 );
-//logger.info('shields at 99%');
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
-
